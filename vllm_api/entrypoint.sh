8657f3c460fc3de6f15f07943a3a229699f7272b
ThuanNaN <duongthuan1445@gmail.com>
2025-05-05 18:40:25 +0000

    Update engine_args

--- vllm_api/entrypoint.sh
+++ vllm_api/entrypoint.sh
@@ -12,14 +12,16 @@ echo "Using Python executable: $PYTHON_CMD"
 
 # Start vllm in the background
 $PYTHON_CMD -m vllm.entrypoints.openai.api_server \
-  --model meta-llama/Llama-3.2-1B-Instruct \
+  --model thuanan/Llama-3.2-1B-Instruct-Chat-sft \
+  --compilation-config {"cache_dir": "../cache"} \
   --port 8000 \
   --quantization bitsandbytes \
   --enable-prefix-caching \
   --swap-space 16 \
   --gpu-memory-utilization 0.9 \
   --disable-log-requests \
-  --max-model-len 4096 \
+  --enable-sleep-mode \
+  --max-model-len 8192 \
   --enable-lora &
 
 # Store the PID of the background process
@@ -30,7 +32,7 @@ wait_for_api() {
   echo "Waiting for vLLM API to be ready..."
   while ! curl -s -H "Authorization: Bearer $VLLM_API_KEY" http://localhost:8000/v1/models > /dev/null; do
     echo "API not ready yet, waiting..."
-    sleep 5
+    sleep 10
   done
   echo "vLLM API is ready!"
 }
@@ -41,7 +43,7 @@ wait_for_api
 # Run the script to load lora adapters
 echo "Loading LoRA adapters..."
 export VLLM_API_KEY=$VLLM_API_KEY
-bash /app/adapters.sh
+# bash /app/adapters.sh
 
 # Wait for the vllm process
 wait $VLLM_PID
